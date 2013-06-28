f0be7dcad0adfd6cc73013333e131b0bfe2c4ac3
ligi <ligi@ligi.de>
2013-06-28 13:52:50 +0200

    add more stuff

---
+++ src/java/org/ligi/androidhelper/adapters/LinkWithDescription.java
@@ -0,0 +1,18 @@
+package org.ligi.androidhelper.adapters;
+
+public class LinkWithDescription {
+	private String url;
+	private String description;
+	public LinkWithDescription(String _url,String _description) {
+		url=_url;
+		description=_description;
+	}
+	
+	public String getURL() {
+		return url;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+}
