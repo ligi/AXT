b52b5b87e8f1e4d82ec65268b22d3910403d906f
ligi <ligi@ligi.de>
2013-08-23 22:48:49 +0200

    add more tests

--- src/test/java/org/ligi/androidhelper/test/TheArrayHelper.java
+++ src/test/java/org/ligi/androidhelper/test/TheArrayHelper.java
@@ -10,10 +10,32 @@ import static org.fest.assertions.Assertions.assertThat;
 public class TheArrayHelper {
 
     @Test
-    public void should_proper_combine_arrays() {
+    public void should_proper_combine_byte_arrays() {
         Byte[] test=AndroidHelper.at(new Byte[] { 23,5}).combineWith(new Byte[] { 42 ,6});
 
         assertThat(test).isEqualTo(new Byte[] { 23,5,42,6});
+    }
+
+    @Test
+    public void should_proper_combine_string_arrays() {
+        String[] test=AndroidHelper.at(new String[] { "foo","bar"}).combineWith(new String [] { "ftw"});
+
+        assertThat(test).isEqualTo(new String[] { "foo" , "bar", "ftw"});
+    }
+
+    @Test
+    public void should_work_even_on_empty_arrays() {
+        Object[] test=AndroidHelper.at(new Object[] {}).combineWith(new Object [] {});
+
+        assertThat(test).isEqualTo(new Object[] {});
+    }
+
+
+    @Test
+    public void should_work_when_one_side_is_empty() {
+        String[] test=AndroidHelper.at(new String[] {}).combineWith(new String [] { "lol"});
+
+        assertThat(test).isEqualTo(new String[] { "lol"});
     }
 
 }
