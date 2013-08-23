c69210974f8fc9aa2442c5f350adb39df090b8f4
ligi <ligi@ligi.de>
2013-08-23 22:48:36 +0200

    indent

--- src/java/org/ligi/androidhelper/views/SquareImageView.java
+++ src/java/org/ligi/androidhelper/views/SquareImageView.java
@@ -7,20 +7,20 @@ import android.widget.ImageView;
 
 public class SquareImageView extends ImageView {
 
-	public SquareImageView(Context context) {
-		super(context);
-	}
+    public SquareImageView(Context context) {
+        super(context);
+    }
 
-	public SquareImageView(Context context,AttributeSet attrs) {
-		super(context,attrs);
-	}
-	
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-	    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-	    	    int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
-	    int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
-	    int size = Math.min(parentWidth, parentHeight);
-	    this.setMeasuredDimension(size,size);
-	}
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int size = Math.min(parentWidth, parentHeight);
+        this.setMeasuredDimension(size, size);
+    }
 }
