bd4aa4260895b5a042837f5cfa163c8238aa21c2
ligi <ligi@ligi.de>
2013-09-04 22:07:28 +0200

    add method to show the keyboard

--- src/java/org/ligi/androidhelper/helpers/ViewHelper.java
+++ src/java/org/ligi/androidhelper/helpers/ViewHelper.java
@@ -28,6 +28,15 @@ public class ViewHelper {
         }
     }
 
+    public void showKeyboard() {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.showSoftInput(view, 0);
+        } else {
+            Log.w("AndroidHelper", "could not show Keyboard as INPUT_METHOD_SERVICE is not available");
+        }
+    }
+
     public void startIntentOnClick(final Intent intent) {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
