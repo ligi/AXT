a18f63d636e30233bd4372de362a2f88c2dde839
ligi <ligi@ligi.de>
2013-12-31 06:15:01 +0100

    indent

--- src/java/org/ligi/axt/helpers/URLHelper.java
+++ src/java/org/ligi/axt/helpers/URLHelper.java
@@ -12,40 +12,40 @@ public class URLHelper {
     private final URL url;
 
     public URLHelper(URL url) {
-        this.url=url;
+        this.url = url;
     }
 
-     public String downloadToString() {
+    public String downloadToString() {
 
-                URLConnection con;
-                InputStream in = null;
-                try {
-                        con = url.openConnection();
-                        in = con.getInputStream();
-                } catch (IOException e2) {
-                        // TODO Auto-generated catch block
-                        e2.printStackTrace();
-                }
-
-                BufferedReader reader = null;
-                try {
-                        reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-                } catch (Exception e3) {
-                        e3.printStackTrace();
-                        return null;
-                }
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-
-                try {
-                        while ((line = reader.readLine()) != null) {
-                                sb.append(line + "\n");
-                        }
-                } catch (IOException e3) {
-                }
-
-                return sb.toString();
-
+        URLConnection con;
+        InputStream in = null;
+        try {
+            con = url.openConnection();
+            in = con.getInputStream();
+        } catch (IOException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
         }
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        } catch (Exception e3) {
+            e3.printStackTrace();
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e3) {
+        }
+
+        return sb.toString();
+
+    }
 
 }
