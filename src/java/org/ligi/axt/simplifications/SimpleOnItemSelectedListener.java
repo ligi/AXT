60dbc541fd69de77d41185f8c3fb9f78ad49fca1
ligi <ligi@ligi.de>
2014-08-15 14:52:21 +0200

    add SimpleOnItemSelectedListener

---
+++ src/java/org/ligi/axt/simplifications/SimpleOnItemSelectedListener.java
@@ -0,0 +1,12 @@
+package org.ligi.axt.simplifications;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public abstract class SimpleOnItemSelectedListener implements OnItemSelectedListener {
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
